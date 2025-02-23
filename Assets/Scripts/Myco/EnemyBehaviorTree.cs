d5daf478805b869f73cd0f61081509a092f8e04c
zhk <148607220+xzhkx@users.noreply.github.com>
2025-02-23 21:27:00 +0700

    Touch Up Enemy AI

---
+++ Assets/Scripts/Myco/EnemyBehaviorTree.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+using zhk.BehaviourTree;
+
+namespace zhk.EnemyBehaviorTree
+{
+    public class IdleStateNode : Node
+    {
+        private EnemyDetectPlayerInRange detectPlayerInRange;
+        private EnemyAnimatorControl enemyAnimatorControl;
+        private EnemyIdle enemyIdle;
+
+        public IdleStateNode(EnemyAnimatorControl enemyAnimatorControl, 
+            EnemyDetectPlayerInRange detectPlayerInRange, EnemyIdle enemyIdle)
+        {
+            this.detectPlayerInRange = detectPlayerInRange;
+            this.enemyAnimatorControl = enemyAnimatorControl;
+            this.enemyIdle = enemyIdle;
+        }
+
+        public override NodeState Execute()
+        {
+            if (detectPlayerInRange.IsPlayerInSightRange())
+            {
+                return NodeState.FAILURE;
+            }
+            else
+            {
+                enemyAnimatorControl.SetTrigger("IdleTrigger");
+                if (enemyIdle.Rest())
+                {
+                    return NodeState.SUCCESS;
+                }
+                return NodeState.RUNNING;
+            }
+        }
+    }
+
+    public class RandomMovementNode : Node
+    {
+        private EnemyDetectPlayerInRange detectPlayerInRange;
+        private EnemyIdle enemyIdle;
+
+        public RandomMovementNode(EnemyIdle enemyIdle, EnemyDetectPlayerInRange detectPlayerInRange)
+        {
+            this.enemyIdle = enemyIdle;
+            this.detectPlayerInRange = detectPlayerInRange;
+        }
+
+        public override NodeState Execute()
+        {
+            if (detectPlayerInRange.IsPlayerInSightRange())
+            {
+                return NodeState.FAILURE;
+            }
+            else
+            {
+                if (enemyIdle.RandomMovement())
+                {
+                    return NodeState.SUCCESS;
+                }
+                return NodeState.RUNNING;
+            }
+        }
+    }
+
+    public class ChasePlayerNode : Node
+    {
+        private EnemyDetectPlayerInRange detectPlayerInRange;
+        private EnemyChasePlayer enemyChasePlayer;
+
+        public ChasePlayerNode(EnemyChasePlayer enemyChasePlayer, EnemyDetectPlayerInRange detectPlayerInRange)
+        {
+            this.detectPlayerInRange = detectPlayerInRange;
+            this.enemyChasePlayer = enemyChasePlayer;
+        }
+
+        public override NodeState Execute()
+        {
+            if (detectPlayerInRange.IsPlayerInAttackRange())
+            {
+                return NodeState.FAILURE;
+            }
+            if (detectPlayerInRange.IsPlayerInSightRange())
+            {
+                enemyChasePlayer.ChasePlayer();
+                return NodeState.RUNNING;
+            }
+            return NodeState.SUCCESS;
+        }
+    }
+
+    public class AttackPlayerNode : Node
+    {
+        private EnemyDetectPlayerInRange detectPlayerInRange;
+        private EnemyAnimatorControl enemyAnimatorControl;
+
+        public AttackPlayerNode(EnemyAnimatorControl enemyAnimatorControl, EnemyDetectPlayerInRange detectPlayerInRange)
+        {
+            this.detectPlayerInRange = detectPlayerInRange;
+            this.enemyAnimatorControl = enemyAnimatorControl;
+        }
+
+        public override NodeState Execute()
+        {
+            if (detectPlayerInRange.IsPlayerInAttackRange())
+            {
+                enemyAnimatorControl.SetTrigger("AttackTrigger");
+                return NodeState.SUCCESS;
+            }
+            else
+            {
+                return NodeState.FAILURE;
+            }
+        }
+    }
+
+    public class RestAttackNode : Node
+    {
+        private EnemyDetectPlayerInRange detectPlayerInRange;
+        private EnemyAnimatorControl enemyAnimatorControl;
+        private EnemyIdle enemyIdle;
+
+        public RestAttackNode(EnemyAnimatorControl enemyAnimatorControl, 
+            EnemyDetectPlayerInRange detectPlayerInRange, EnemyIdle enemyIdle)
+        {
+            this.detectPlayerInRange = detectPlayerInRange;
+            this.enemyAnimatorControl = enemyAnimatorControl;
+            this.enemyIdle = enemyIdle;
+        }
+
+        public override NodeState Execute()
+        {
+            if (enemyIdle.Rest())
+            {
+                return NodeState.SUCCESS;
+            }
+            else
+            {
+                enemyAnimatorControl.SetTrigger("IdleTrigger");
+                return NodeState.RUNNING;
+            }
+        }
+    }
+}
--- Assets/Scripts/Myco/MycoAI.cs
+++ Assets/Scripts/Myco/MycoAI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using zhk.BehaviourTree;
+using zhk.EnemyBehaviorTree;
 
 public class MycoAI : MonoBehaviour
 {
@@ -21,131 +22,24 @@ public class MycoAI : MonoBehaviour
         RandomMovementNode randomMovementNode = new RandomMovementNode(enemyIdle, enemyDetectPlayerInRange);
         ChasePlayerNode chasePlayerNode = new ChasePlayerNode(enemyChasePlayer, enemyDetectPlayerInRange);
         AttackPlayerNode attackPlayerNode = new AttackPlayerNode(enemyAnimatorControl, enemyDetectPlayerInRange);
+        RestAttackNode restAttackNode = new RestAttackNode(enemyAnimatorControl, enemyDetectPlayerInRange, enemyIdle);
 
         MycoAIRootNode = new FallbackNode(3);
         SequenceNode EnemyMovementNode = new SequenceNode(2);
+        SequenceNode EnemyAttackNode = new SequenceNode(2);
 
         EnemyMovementNode.AttachNode(idleStateNode);
         EnemyMovementNode.AttachNode(randomMovementNode);
+        EnemyAttackNode.AttachNode(attackPlayerNode);
+        EnemyAttackNode.AttachNode(restAttackNode);
+
         MycoAIRootNode.AttachNode(EnemyMovementNode);
         MycoAIRootNode.AttachNode(chasePlayerNode);
-        MycoAIRootNode.AttachNode(attackPlayerNode);
+        MycoAIRootNode.AttachNode(EnemyAttackNode);
     }
 
     private void Update()
     {
         MycoAIRootNode.Execute();
-    }
-
-    private class IdleStateNode : Node
-    {
-        private EnemyDetectPlayerInRange detectPlayerInRange;
-        private EnemyAnimatorControl enemyAnimatorControl;
-        private EnemyIdle enemyIdle;
-
-        public IdleStateNode(EnemyAnimatorControl enemyAnimatorControl, EnemyDetectPlayerInRange detectPlayerInRange
-            ,EnemyIdle enemyIdle)
-        {
-            this.detectPlayerInRange = detectPlayerInRange;
-            this.enemyAnimatorControl = enemyAnimatorControl;
-            this.enemyIdle = enemyIdle;
-        }
-
-        public override NodeState Execute()
-        {
-            Debug.Log("IdleState");
-            if (detectPlayerInRange.IsPlayerInSightRange())
-            {
-                return NodeState.FAILURE;
-            }
-            else
-            {
-                enemyAnimatorControl.SetTrigger("IdleTrigger");
-                if (enemyIdle.Rest())
-                {
-                    return NodeState.SUCCESS;
-                }
-                return NodeState.RUNNING;
-            }
-        }
-    } 
-
-    private class RandomMovementNode : Node
-    {
-        private EnemyDetectPlayerInRange detectPlayerInRange;
-        private EnemyIdle enemyIdle;
-
-        public RandomMovementNode(EnemyIdle enemyIdle, EnemyDetectPlayerInRange detectPlayerInRange)
-        {
-            this.enemyIdle = enemyIdle;
-            this.detectPlayerInRange = detectPlayerInRange;
-        }
-
-        public override NodeState Execute()
-        {
-            if (detectPlayerInRange.IsPlayerInSightRange())
-            {
-                return NodeState.FAILURE;
-            }
-            else
-            {
-                if (enemyIdle.RandomMovement())
-                {
-                    return NodeState.SUCCESS;
-                }
-                return NodeState.RUNNING;
-            }
-        }
-    }
-
-    private class ChasePlayerNode : Node
-    {
-        private EnemyDetectPlayerInRange detectPlayerInRange;
-        private EnemyChasePlayer enemyChasePlayer;
-
-        public ChasePlayerNode(EnemyChasePlayer enemyChasePlayer, EnemyDetectPlayerInRange detectPlayerInRange)
-        {
-            this.detectPlayerInRange = detectPlayerInRange;
-            this.enemyChasePlayer = enemyChasePlayer; 
-        }
-
-        public override NodeState Execute()
-        {
-            if(detectPlayerInRange.IsPlayerInAttackRange())
-            {
-                return NodeState.FAILURE;
-            }
-            if (detectPlayerInRange.IsPlayerInSightRange())
-            {
-                enemyChasePlayer.ChasePlayer();
-                return NodeState.RUNNING;
-            } 
-            return NodeState.SUCCESS;
-        }
-    }
-
-    private class AttackPlayerNode : Node
-    {
-        private EnemyDetectPlayerInRange detectPlayerInRange;
-        private EnemyAnimatorControl enemyAnimatorControl;
-
-        public AttackPlayerNode(EnemyAnimatorControl enemyAnimatorControl, EnemyDetectPlayerInRange detectPlayerInRange)
-        {
-            this.detectPlayerInRange = detectPlayerInRange;
-            this.enemyAnimatorControl = enemyAnimatorControl;
-        }
-
-        public override NodeState Execute()
-        {
-            if (detectPlayerInRange.IsPlayerInAttackRange())
-            {
-                enemyAnimatorControl.SetTrigger("AttackTrigger");
-                return NodeState.RUNNING; //-> sequence rest
-            }
-            else
-            {
-                return NodeState.FAILURE;
-            }
-        }
     }
 }
